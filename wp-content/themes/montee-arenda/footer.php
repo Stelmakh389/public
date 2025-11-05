f6c8827949f1859a175fdaeff081abaabe313dcf
Stelmakh389 <stelmakh389@gmail.com>
2025-11-05 16:27:06 +0300

    Второй коммит

--- wp-content/themes/montee-arenda/footer.php
+++ wp-content/themes/montee-arenda/footer.php
@@ -22,6 +22,34 @@
 	</div>
 </footer>
 
+<script src="https://unpkg.com/imask"></script>
+
+<!-- Попап форма обратной связи -->
+<div class="popup__bg" id="contact-popup">
+	<div class="popup">
+		<div class="close-popup popup-popup">
+			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M18 6L6 18M6 6L18 18" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+			</svg>
+		</div>
+		<h3 class="popup__title" contenteditable="false">Оставить заявку</h3>
+		<p class="popup__subtitle" contenteditable="false">Мы свяжемся с вами в течение 15 минут</p>
+		<form class="popup__form" id="contact-form">
+			<div class="popup__input-wrapper">
+				<input type="tel" name="phone" class="popup__input phone" placeholder="+7(999)999-99-99" required="">
+			</div>
+			<div class="popup__checkbox-wrapper">
+				<label class="popup__checkbox-label">
+					<input type="checkbox" name="privacy" class="popup__checkbox" required="">
+					<span class="popup__checkbox-custom"></span>
+					<span class="popup__checkbox-text" contenteditable="false">Я согласен с условиями <a href="privacy.html" target="_blank" class="" contenteditable="false">обработки персональных данных</a></span>
+				</label>
+			</div>
+			<button type="submit" class="btn btn-primary popup__submit">Отправить заявку</button>
+		</form>
+	</div>
+</div>
+
 <?php wp_footer(); ?>
 
 </body>
